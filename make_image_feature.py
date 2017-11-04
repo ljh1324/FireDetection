bd3752f49eb2aaa1686354d02eb7806ef1e71b3a
jhl <ljhw3377@gmail.com>
2017-11-05 07:56:13 +0900

    0.025 add location feature in make_image_feature

--- make_image_feature.py
+++ make_image_feature.py
@@ -27,32 +27,40 @@ def img_to_numpy(img_file, size):
     return img, feature
 
 
-def make_fixed_image_feature_in_dir(dir, size, save_img_dir, feature_file_name):
+def make_fixed_image_feature_in_dir(dir, size, save_img_dir, feature_file_name, location_file_name):
     if not os.path.exists(save_img_dir):
         os.makedirs(save_img_dir)
 
     if not os.path.exists(os.path.dirname(feature_file_name)):
         os.makedirs(os.path.dirname(feature_file_name))
 
+    if not os.path.exists(os.path.dirname(location_file_name)):
+        os.makedirs(os.path.dirname(location_file_name))
+
     images = os.listdir(dir)
 
     feature_list = []
+    location_list = []
 
     idx = 1
     for image in images:
         img, feature = img_to_numpy(join(dir, image), size)
         img.save(join(save_img_dir, str(idx) + '.jpg'))
         feature_list.append(feature)
+        location_list.append([0, 0, 0, 0])                      # non-fired
         idx += 1
 
     feature_list = np.array(feature_list)
+    location_list = np.array(location_list)
     print(feature_list.size)
     np.save(feature_file_name, feature_list)
+    np.save(location_file_name, location_list)
 
 
 if __name__ == '__main__':
     dir = 'data/view'
     save_img_dir = 'data/resized_view'
     feature_file_name = 'data/view_feature/view_feature.npy'
+    location_file_name = 'data/view_location/location_feature.npy'
     size = (256, 256)
-    make_fixed_image_feature_in_dir(dir, size, save_img_dir, feature_file_name)
+    make_fixed_image_feature_in_dir(dir, size, save_img_dir, feature_file_name, location_file_name)
